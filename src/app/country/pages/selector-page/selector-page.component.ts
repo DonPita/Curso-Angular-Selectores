9a52dcb2e3b542f8e4c9ac64fd52f37dd26e3109
Adrian Pita <pitabermudez@gmail.com>
2024-05-21 10:18:47 +0200

    Para que aparezcan las fronteras en el HTML

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -18,15 +18,17 @@ export class SelectorPageComponent implements OnInit {
   ) { }
 
   public countriesByRegion: SmallCountry[] = [];
+  public borders: string[] = [];
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     countries: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   })
 
   ngOnInit(): void {
     this.onRegionChanged();
+    this.onCountryChanged();
   }
 
   get regions(): Region[] {
@@ -34,10 +36,11 @@ export class SelectorPageComponent implements OnInit {
   }
 
   //Metodo para tener el OnInit lo mas legible posible
-  private onRegionChanged(): void {
+  onRegionChanged(): void {
     this.myForm.get('region')!.valueChanges
       .pipe(
-        tap( () => this.myForm.get('countries')!.setValue('') ), //Poner el valor a string vacio, cuando haces el cambio de region
+        tap(() => this.myForm.get('countries')!.setValue('')), //Poner el valor a string vacio, cuando haces el cambio de region
+        tap(() => this.borders = []), //Poner el valor a string vacio, cuando haces el cambio de region
         switchMap(region => this.countryService.getCountriesByRegion(region)),
       )
       .subscribe(countries => {
@@ -45,5 +48,22 @@ export class SelectorPageComponent implements OnInit {
           c1.name.localeCompare(c2.name)) //Ordenar los paises alfabeticamente.
       })
   }
+
+  onCountryChanged(): void {
+    this.myForm.get('countries')!.valueChanges
+      .pipe(
+        tap(() => this.myForm.get('border')!.setValue('')), //Poner el valor a string vacio, cuando haces el cambio de region
+        filter((value: string) => value.length > 0),
+        switchMap(alphacode => this.countryService.getCountryByAlphaCode(alphacode)),
+      )
+      .subscribe(country => {
+        console.log({ borders: country.borders })
+        this.borders = country.borders;
+      })
+    // .subscribe(countries => {
+    //   this.countriesByRegion = countries.sort((c1, c2) =>
+    //     c1.name.localeCompare(c2.name)) //Ordenar los paises alfabeticamente.
+  }
 }
+
 
